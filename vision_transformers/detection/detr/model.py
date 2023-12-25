bd830d03597a614346fe0606ac21df26d941962a
Tommy Ngx <tommyfederation@gmail.com>
2023-12-25 11:32:35 +1100

    Update model.py

--- vision_transformers/detection/detr/model.py
+++ vision_transformers/detection/detr/model.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import warnings
 
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 class DETRModel(nn.Module):
     def __init__(self, num_classes=1, model='detr_resnet50'):
         super(DETRModel, self).__init__()
