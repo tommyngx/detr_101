89790ef1367ae9e4f936831a3b7220c29367d823
Tommy Ngx <tommyfederation@gmail.com>
2024-01-14 08:50:50 +1100

    Update transforms.py

--- tools/utils/detection/detr/transforms.py
+++ tools/utils/detection/detr/transforms.py
@@ -48,7 +48,8 @@ def get_train_transform():
         A.Normalize(max_pixel_value=1),
         ToTensorV2(p=1.0),
     ], bbox_params=A.BboxParams(
-        format='pascal_voc',min_area=60, min_visibility=80,label_fields=['labels']),
+        #format='pascal_voc',min_area=60, min_visibility=80,label_fields=['labels']),
+        format='pascal_voc', min_visibility=100,label_fields=['labels']),
         #format='pascal_voc', label_fields=['labels'])
 
     )
